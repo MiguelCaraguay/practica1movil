6499d3a6083e487d62af198ea1906b8bce627dcc
miguelcaraguay <miguelcaraguay.1502@gmail.com>
2022-10-27 20:57:02 -0500

    practica 1

--- app/src/main/java/com/example/practica/MainActivity.kt
+++ app/src/main/java/com/example/practica/MainActivity.kt
@@ -7,9 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // validarEdadPersona()
-        // tablaMultiplicar()
-        // calcularIva()
+        validarEdadPersona()
+        tablaMultiplicar()
+        listaPlataformas()
+        vehiculo()
+        calcularIva()
         validacionCedula()
     }
 }
@@ -18,8 +20,9 @@ class MainActivity : AppCompatActivity() {
 - Presentar las propiedades de un vehículo utilizando clases, como tracción, motor, tipo de vehículo, capacidad
 */
 
-// - Validar si una persona es mayor o menor de edad
 
+
+// - Validar si una persona es mayor o menor de edad
 fun validarEdadPersona(){
 
     var edad = 18
@@ -33,19 +36,79 @@ fun validarEdadPersona(){
 
 // - Presentar la tabla de multiplicar de n numero de forma ascendente y descendente.
 fun tablaMultiplicar(){
-    var numero = 4
+    var numero = 6
+    var tabla = arrayListOf("")
+    var cadena = ""
     println("Tabla de multiplicar de numero $numero")
-    for(num in 1..numero ){
+    println("Tabla ascendente")
+
+    for(num in 1..12 ){
         var result =  numero * num
-        println("$numero * $num = $result")
+        cadena = "$numero * $num = $result"
+        tabla.add(cadena)
+    }
+    println(cadena)
+    var tablaDescendente = tabla.asReversed()
+    println("Tabla descendente")
+    for(item in tablaDescendente){
+        println(item)
     }
 }
 
 //- Mostrar el listado de paralelo de la materia de Plataformas móviles y los
 // subgrupos por proyectos (Mostrar los resultados ordenados).
+fun listaPlataformas(){
+    val nombres = mapOf(
+        "Eric Alvarado" to "P1",
+        "Miguel Caraguay" to "P1",
+        "Carlos Castillo" to "P2",
+        "Erick Cuenca" to "P2",
+        "Anthony Espinoza" to "P1",
+        "Jhoselin Guachizaca" to "P1",
+        "Steven Jara" to "P1",
+        "Sofia Jaramillo" to "P1",
+        "Andres Jimenez" to "P2",
+        "Diego Leiva" to "P1",
+        "Mateo Martinez" to "P2",
+        "Pablo Montaño" to "P1",
+        "Rommel Ojeda" to "P2",
+        "Joselito Ordeoñez" to "P1",
+        "Bryan Rodriguez" to "P2",
+        "Pablo Saraguro" to "P2",
+        "Isaias Silva" to "P1")
 
+    println("Listado de paralelo")
+    for(item in nombres.toSortedMap()){
+        println(item.key)
+    }
+
+    var grupos = nombres.toSortedMap()
+    println("\nListado de grupos")
+    for(item in grupos){
+        println("${item.key} -> ${item.value}")
+    }
+}
+
+
+// - Presentar las propiedades de un vehículo utilizando clases, como tracción, motor, tipo de vehículo, capacidad
+class Vehiculo {
+    var marca: String = ""
+    var anio: Int = 0
+    var tipoVehiculo: String = ""
+    var placa: String =""
+}
+
+fun vehiculo(){
+    var vehiculo = Vehiculo()
+    vehiculo.marca = "Hyundai"
+    vehiculo.anio = 2012
+    vehiculo.tipoVehiculo = "Particular"
+    vehiculo.placa= "LBB-1234"
+    var cadena = "Caracteristicas del vehiculo:\nMarca:${vehiculo.marca}\nAÑO:${vehiculo.anio}"+
+            "\nTipo:${vehiculo.tipoVehiculo}\nPlaca:${vehiculo.placa}"
+    println(cadena)
+}
 // calculo del 12% del iva
-
 fun calcularIva(){
     var total = 100
     var iva = total * 0.12
@@ -53,27 +116,29 @@ fun calcularIva(){
 }
 
 // validacion de la cedula
-
 fun validacionCedula(){
-    val cedula = listOf(1,1,0,4,8,1,3,2,5,6)
+    val cedula = listOf(1,1,0,5,7,2,1,9,1,2)
     val validacion = listOf(2,1,2,1,2,1,2,1,2)
-
 
     var result = 0
 
-    for (i in 0..validacion.size){
+    for (i in 0..validacion.size-1){
         var aux = cedula[i] * validacion[i]
-        if(aux > 10){
-            var values = aux.toString().split("")
-            var newlist = values.subList(1,3)
-            for
-
-            println("meme ${newlist}, $aux")
+        if(aux >= 10){
+            aux = aux - 9
         }
-        println(aux)
-
         result += aux
     }
-    println("resultado $result")
 
+    var valorRestar = ((result/10)+1)*10
+    var valorComprobarCedula = cedula.last()
+    var resultadoResta = valorRestar - result
+
+    println("resultado $result")
+    println("resta de $result - $valorRestar")
+    if(valorComprobarCedula == resultadoResta){
+        println(" $valorComprobarCedula == $resultadoResta Cedula valida" )
+    }else{
+        println("Cedula invalida")
+    }
 }
